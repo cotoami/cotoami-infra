b1a8b99cbf0aaa44ee2a3166ffc229e3ee6fbf71
Daisuke Morita <daisuke.marubinotto@gmail.com>
2016-11-29 19:26:48 +0900

    Add a hosted zone: dev.cotoa.me

--- terraform/aws/common/common.tf
+++ terraform/aws/common/common.tf
@@ -5,3 +5,20 @@ provider "aws" {
 resource "aws_route53_zone" "main" {
   name = "cotoa.me"
 }
+
+resource "aws_route53_zone" "dev" {
+  name = "dev.cotoa.me"
+}
+
+resource "aws_route53_record" "dev_ns" {
+  zone_id = "${aws_route53_zone.dev.zone_id}"
+  name = "dev.cotoa.me"
+  type = "NS"
+  ttl = "30"
+  records = [
+    "${aws_route53_zone.dev.name_servers.0}",
+    "${aws_route53_zone.dev.name_servers.1}",
+    "${aws_route53_zone.dev.name_servers.2}",
+    "${aws_route53_zone.dev.name_servers.3}"
+  ]
+}
