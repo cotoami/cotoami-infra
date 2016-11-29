eb4b8e857590d4c1571c6555edff463b9827cea0
Daisuke Morita <daisuke.marubinotto@gmail.com>
2016-11-30 00:54:56 +0900

    Deploy an app to kubernetes cluster

--- terraform/aws/common/common.tf
+++ terraform/aws/common/common.tf
@@ -35,3 +35,14 @@ resource "aws_route53_record" "dev_ns" {
     "${aws_route53_zone.dev.name_servers.3}"
   ]
 }
+
+resource "aws_route53_record" "www" {
+  zone_id = "${aws_route53_zone.main.zone_id}"
+  name = "cotoa.me"
+  type = "A"
+  alias {
+    name = "a630e3f1ab64711e6bf8506314825cf4-310288189.ap-northeast-1.elb.amazonaws.com"
+    zone_id = "Z14GRHDCWA56QT"
+    evaluate_target_health = true
+  }
+}
