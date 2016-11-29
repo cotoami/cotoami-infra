bedd62c9edd3c8a4597a4feef8cb7e0a96a898d1
Daisuke Morita <daisuke.marubinotto@gmail.com>
2016-11-30 00:13:20 +0900

    Tweak route 53 settings

--- terraform/aws/common/common.tf
+++ terraform/aws/common/common.tf
@@ -10,6 +10,19 @@ resource "aws_route53_zone" "dev" {
   name = "dev.cotoa.me"
 }
 
+resource "aws_route53_record" "main_dev_ns" {
+  zone_id = "${aws_route53_zone.main.zone_id}"
+  name = "dev.cotoa.me"
+  type = "NS"
+  ttl = "30"
+  records = [
+    "${aws_route53_zone.dev.name_servers.0}",
+    "${aws_route53_zone.dev.name_servers.1}",
+    "${aws_route53_zone.dev.name_servers.2}",
+    "${aws_route53_zone.dev.name_servers.3}"
+  ]
+}
+
 resource "aws_route53_record" "dev_ns" {
   zone_id = "${aws_route53_zone.dev.zone_id}"
   name = "dev.cotoa.me"
