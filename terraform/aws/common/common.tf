6660bf15cf5ad78a27df2957bfc7de521d392c55
Daisuke Morita <daisuke.marubinotto@gmail.com>
2016-12-01 19:31:42 +0900

    Add a hosted zone for k8s.cotoa.me

--- terraform/aws/common/common.tf
+++ terraform/aws/common/common.tf
@@ -6,6 +6,25 @@ resource "aws_route53_zone" "main" {
   name = "cotoa.me"
 }
 
+resource "aws_route53_zone" "k8s" {
+  name = "k8s.cotoa.me"
+}
+
+resource "aws_route53_record" "main_k8s_ns" {
+  zone_id = "${aws_route53_zone.main.zone_id}"
+  name = "k8s.cotoa.me"
+  type = "NS"
+  ttl = "30"
+  records = [
+    "${aws_route53_zone.k8s.name_servers.0}",
+    "${aws_route53_zone.k8s.name_servers.1}",
+    "${aws_route53_zone.k8s.name_servers.2}",
+    "${aws_route53_zone.k8s.name_servers.3}"
+  ]
+}
+
+# ----
+
 resource "aws_route53_zone" "dev" {
   name = "dev.cotoa.me"
 }
